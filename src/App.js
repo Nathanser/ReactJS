32ec663a062f7788b11d067bb78428dbeec19d11
Nathanser <nathan.serror@example.com>
2024-11-27 14:36:41 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Wolrd ! Nathan
+          Hello Wolrd ! Nathan SERROR
         </p>
         <a
           className="App-link"
