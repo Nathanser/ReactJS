f633021ec6edc41e127d6c11337fbf3d697f0282
nathan <nathan.serror@gmail.com>
2025-03-05 16:29:54 +0100

    dev

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React !! N.S
+          Learn React !!
         </a>
       </header>
     </div>
